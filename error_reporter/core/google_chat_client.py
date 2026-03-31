aa1c108adfd8b097ce707dd7880d3e9218d1d4af
Seungkyu-Han <trust1204@gmail.com>
2026-03-31 23:09:14 +0900

    feat: add google chat client

---
+++ error_reporter/core/google_chat_client.py
@@ -0,0 +1,5 @@
+from error_reporter.core.slack_client import SlackClient
+
+
+class GoogleChatClient(SlackClient):
+    ...
--- error_reporter/error_reporter_options.py
+++ error_reporter/error_reporter_options.py
@@ -2,8 +2,12 @@ from pydantic import BaseModel
 
 class SlackOptions(BaseModel):
     webhook_url: str
-    server_name: str = "unknown"
+    server_name: str = "unknown server"
 
 class DiscordOptions(BaseModel):
     webhook_url: str
-    server_name: str = "unknown"
+    server_name: str = "unknown server"
+
+class GoogleChatOptions(BaseModel):
+    webhook_url: str
+    server_name: str = "unknown server"
